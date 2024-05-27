3497032ab6def2d3b114a054fad9f66eb2602f89
Builtman <93301487+Builtman@users.noreply.github.com>
2024-05-27 11:32:17 +0900

    Add files via upload

---
+++ streamlit_01.py
@@ -0,0 +1,21 @@
+# pip install streamlit 역시나 안됨
+# streamlit run streamlit_01.py
+import streamlit as st
+import pandas as pd
+
+# st.write('Hello, *world* :Sunglasses:')
+# st.write(1234)
+# df= pd.DataFrame({'first column' : [ 1,2,3,4],
+#                   'second column' : [10,20,30,40]})
+
+# st.write(df)
+
+# st.header('st.button')
+
+# if st.button('say hello') :
+#     st.write('why hello there')
+# else :
+#     st.write('Goodbye')
+
+st.write("# chart view")
+st.write(" raw data")
